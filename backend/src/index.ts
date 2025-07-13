bd1642796afed533d12302574982e7cf76c65fee
Jeffrey Freckleface Cogswell <jeffcogs@gmail.com>
2025-07-12 22:20:11 -0600

    Updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,28 +4,17 @@ import { sendSampleData } from './dataService.js';
 const app = express();
 const port = 3000;
 
-let item:any = {
-  "data_value": "A lunar revolution with libertarian flair",
-  "annotations": {
-    "type": "book",
-    "title": "The Moon is a Harsh Mistress",
-    "author": "Robert A. Heinlein",
-    "genre": "science fiction",
-    "rating": 5,
-    "owned": true,
-    "year": 1966
-  }
-};
+app.get('/', (req, res) => {
+    res.send('Hello from Golem-base!');
+});
 
-await sendSampleData();
+app.get('/load-data', async (req, res) => {
+    await sendSampleData()
+    return 'OK';
+})
 
-// Basic route
-// app.get('/', (req, res) => {
-//   res.send('Hello from Freckleface Express!');
-// });
-
-// // Start server
-// app.listen(port, () => {
-//   console.log(`Server is running at http://localhost:${port}`);
-// });
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
 
